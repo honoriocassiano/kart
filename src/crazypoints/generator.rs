d6febad582defde2123f454c54828e84b0d233a0
Cassiano Honorio <honoriocassiano@gmail.com>
2021-04-18 11:42:05 -0300

    Remove proximity constraint to draw the lines

--- src/crazypoints/generator.rs
+++ src/crazypoints/generator.rs
@@ -5,7 +5,7 @@ use svg::Document;
 use std::iter::Iterator;
 
 use crate::common::Particle;
-use crate::common::{Vec2, Vector};
+use crate::common::Vec2;
 
 pub fn generate(width: u32, height: u32, max_it: u32) -> Document {
     let particle1 = Particle::new(Vec2(450.0, 0.0), Vec2(0.0, 20.0));
@@ -22,22 +22,18 @@ pub fn generate(width: u32, height: u32, max_it: u32) -> Document {
             p.update(rng.gen::<f64>() * 3.0 + 1.0);
         });
 
-        let distance = (particles[0].position() - particles[1].position()).length();
+        let p1 = particles[0].position();
+        let p2 = particles[1].position();
 
-        if distance < 160.0 {
-            let p1 = particles[0].position();
-            let p2 = particles[1].position();
+        let line = Line::new()
+            .set("x1", p1.x())
+            .set("y1", p1.y())
+            .set("x2", p2.x())
+            .set("y2", p2.y())
+            .set("stroke", "black")
+            .set("stroke-width", 3);
 
-            let line = Line::new()
-                .set("x1", p1.x())
-                .set("y1", p1.y())
-                .set("x2", p2.x())
-                .set("y2", p2.y())
-                .set("stroke", "black")
-                .set("stroke-width", 3);
-
-            document = document.add(line);
-        }
+        document = document.add(line);
     }
 
     document
