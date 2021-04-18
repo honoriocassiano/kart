c671a937a81ebbb8b317ae627194274dfccaaa4a
Cassiano Honorio <honoriocassiano@gmail.com>
2021-04-17 22:29:15 -0300

    Remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
 use rand::prelude::*;
-// use rand::seq::IteratorRandom;
 
-use svg::Document;
-// use svg::node::element::Path;
-// use svg::node::element::path::Data;
 use svg::node::element::Line;
+use svg::Document;
 
 fn main() {
-    // let data = Data::new()
-    //     .move_to((10, 10))
-    //     .line_by((0, 50))
-    //     .line_by((50, 0))
-    //     .line_by((0, -50))
-    //     .close();
-    //
-    // let path = Path::new()
-    //     .set("fill", "none")
-    //     .set("stroke", "black")
-    //     .set("stroke-width", 3)
-    //     .set("d", data);
-
-    let mut document = Document::new().set("viewBox", (0, 0, 800, 800));
-
     let mut rng = thread_rng();
 
     let x2: i32 = 500;
     let y2: i32 = 500;
     let colors = vec!["black", "orange", "blue", "green", "brown"];
+
+    let mut document = Document::new().set("viewBox", (0, 0, 800, 800));
 
     for _ in 0..100 {
         let x1: i32 = rng.gen_range(0..400);
@@ -36,8 +20,8 @@ fn main() {
 
         let line = Line::new()
             .set("x1", x1)
-            .set("x2", x2)
             .set("y1", y1)
+            .set("x2", x2)
             .set("y2", y2)
             .set("stroke", color)
             .set("stroke-witdh", "2");
